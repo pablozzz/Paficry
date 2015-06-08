3a695db123047c287f9bdbafb1160da9b664ab92
pablozzz <gpvspb@gmail.com>
2015-06-08 11:09:36 +0300

    command line work changed

--- paficry.py
+++ paficry.py
@@ -4,19 +4,11 @@ import sys
 from Crypto.Cipher import AES
 
 #inicialize key and aes mode
+
 key = '11111111111111111111111111111111'
 c_mode = AES.MODE_ECB
 encryptor = AES.new(key, c_mode)
-
-#inicialize files
-
-mode = sys.argv[1]
-input_file_name = sys.argv[2]
-output_file_name = sys.argv[3]
-
-input_file = open(input_file_name, "rb")
-text = input_file.read()
-input_file.close()
+blocksize = 16
 
 def encrypt(text, outfile):
 # encrypt function
@@ -25,7 +17,7 @@ def encrypt(text, outfile):
 	for symbol in text:
 		count += 1
 		block = block + symbol
-		if count == 16:
+		if count == blocksize:
 			cipherblock = encryptor.encrypt(block)
 			output_file = open(outfile, "ab")
 			output_file.write(cipherblock)
@@ -40,25 +32,41 @@ def decrypt(text, outfile):
 	block = ""
 	for symbol in text:
 		count += 1
-        block = block + symbol
-        if count == 16:
-            plainblock = encryptor.decrypt(block)
-            output_file = open(outfile, "ab")
-            output_file.write(plainblock)
-            output_file.close()
-            block = ""
-            count = 0
-    print "done"
+                block = block + symbol
+                if count == blocksize:
+                        plainblock = encryptor.decrypt(block)
+                        output_file = open(outfile, "ab")
+                        output_file.write(plainblock)
+                        output_file.close()
+                        block = ""
+                        count = 0
+        print "done"
 		
 
 #start program
 
-if mode == '-e':
-	print "start encryption"
-	encrypt(text, output_file_name)
-elif mode == '-d':
-	print "start decryption"
-	decrypt(text, output_file_name)
+if len(sys.argv) != 4:
+	print "Usage: python paficry.py (-e or -d) input_file output_file"
+else: 
+
+#inicialize files
+
+	mode = sys.argv[1]
+	input_file_name = sys.argv[2]
+	output_file_name = sys.argv[3]
+	
+	input_file = open(input_file_name, "rb")
+	text = input_file.read()
+	input_file.close()
+ 
+	if mode == '-e':
+		print "start encryption"
+		encrypt(text, output_file_name)
+	elif mode == '-d':
+		print "start decryption"
+		decrypt(text, output_file_name)
+	else: 
+		print "-e - encription mode or -d - decryption mode"
  
 
 
